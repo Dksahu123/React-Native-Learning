f6f4b5341d6940277a896f0745572cb77084bcc8
Dinesh Sahu <109724764+Dksahu123@users.noreply.github.com>
2023-03-18 14:11:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Appnav from './src/Appnav';
+
+
+export default function App() {
+  return (
+  <Appnav />
+  );
+}
+
+
+
+
+
+
+
+
+
